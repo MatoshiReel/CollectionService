e6af7a7287aea3a3e8de84efe4725b08874ed9b4
MatoshixX <maksimpest7@gmail.com>
2026-04-05 12:51:21 +0300

    First initialization.

---
+++ src/test/java/ru/reel/CollectionService/CollectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.reel.CollectionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
